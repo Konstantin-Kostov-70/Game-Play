fdb95c0dcffaa694267818b43a35fb1b5d638412
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-26 21:01:41 +0200

    Create footer component

--- game-2/src/App.js
+++ game-2/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import Footer from "./components/Footer/Footer"
 import { HomePage } from "./components/HomePage/HomePage";
 import { Login } from "./components/Login/Login";
 import { Logout } from "./components/Logout/Logout";
@@ -13,37 +14,35 @@ import { AuthProvider } from "./contexts/authContext";
 import { Profile } from "./components/UserProfile/UserProfile";
 import { UserEdit } from "./components/UserEdit/UserEdit";
 import { ErrorPage } from "./components/Errors/ErrorPage";
-import { NotFound } from "./components/Errors/NotFound"
-
+import { NotFound } from "./components/Errors/NotFound";
 
 function App() {
   return (
-   
-      <AuthProvider>
-        <div id="box">
-          <GameProvider>
-            <Header />
+    <AuthProvider>
+      <div id="box">
+        <GameProvider>
+          <Header />
 
-            <main id="main-content">
-              <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/logout' element={<Logout />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/profile/:id' element={<Profile />} />
-                <Route path='/user/edit/:id' element={<UserEdit />} />
-                <Route path='/details/:id' element={<DetailsPage />} />
-                <Route path='/edit/:id' element={<EditPage />} />
-                <Route path='/create' element={<CreatePage />} />
-                <Route path='/catalog' element={<Catalog />} />
-                <Route path='/errors' element={<ErrorPage />} />
-                <Route path='/notfound' element={<NotFound />} />
-              </Routes>
-            </main>
-          </GameProvider>
-        </div>
-      </AuthProvider>
-   
+          <main id="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/user/edit/:id" element={<UserEdit />} />
+              <Route path="/details/:id" element={<DetailsPage />} />
+              <Route path="/edit/:id" element={<EditPage />} />
+              <Route path="/create" element={<CreatePage />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/errors" element={<ErrorPage />} />
+              <Route path="/notfound" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </GameProvider>
+      </div>
+    </AuthProvider>
   );
 }
 
