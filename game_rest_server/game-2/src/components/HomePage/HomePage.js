9480413fe0119e9c832f87f5fed10804aad16000
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-10 05:04:14 +0200

    change the path to style.css

--- game_rest_server/game-2/src/components/HomePage/HomePage.js
+++ game_rest_server/game-2/src/components/HomePage/HomePage.js
@@ -27,7 +27,8 @@ export const HomePage = () => {
                     <h2>ALL new games are </h2>
                     <h3>Only in GamesPlay</h3>
                 </div>
-                <img src="./images/four_slider_img01.png" alt="hero" />
+                {/* <img src="./images/four_slider_img01.png" alt="hero" /> */}
+                <img src="./static/images/four_slider_img01.png" alt="hero" />
 
                 <div id="home-page">
                     <h1>Latest Games</h1>
