1d4a913fb212090c6602d806929114c0b0619ba7
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-04 20:15:19 +0200

    update settings.py

--- game_rest_server/game_rest_server/settings.py
+++ game_rest_server/game_rest_server/settings.py
@@ -88,23 +88,24 @@ WSGI_APPLICATION = 'game_rest_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+if ENVIRONMENT == 'development':
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': env('DB_ENGINE'),
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': env('DB_PORT'),
-#     }
-# }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        }
+    }
 
 
 # Password validation
